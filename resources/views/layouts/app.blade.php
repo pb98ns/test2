a4ac220d39262014e5c036ef651bfa29216a9826
pb98ns <61550048+pb98ns@users.noreply.github.com>
2022-11-10 13:39:41 +0100

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -144,7 +144,7 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right " aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item " href="{{ route('logout') }}"
+                                    <a class="dropdown-item text-center" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                         {{ __('Wyloguj się') }}
